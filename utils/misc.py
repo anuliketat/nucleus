3799c9d4ff4669d72eddb9e81bcd3ecddc128e21
Chinni Chaitanya <chchaitanya95@gmail.com>
2019-02-22 11:06:22 +0530

    Add logger function

--- utils/misc.py
+++ utils/misc.py
@@ -1,4 +1,26 @@
 from operator import itemgetter
+import datetime
+import traceback
 
 def sort_tuple(data, sort_key, descending=True):
 	return sorted(data, key=itemgetter(sort_key), reverse=descending)
+
+def get_traceback(e):
+    tb = (
+			"Traceback (most recent call last):\n"
+			+ "".join(
+			    traceback.format_list(
+			        traceback.extract_tb(
+			            e.__traceback__
+		            )
+		        )
+		    )
+			+ type(e).__name__
+			+ ": "
+			+ str(e)
+    	)
+    return tb
+
+def logger(product, event_type, event):
+	text = 'UTC: {} | {} | {} | {}'.format(datetime.datetime.utcnow(), str(product), str(event_type), str(event))
+	print(text)
