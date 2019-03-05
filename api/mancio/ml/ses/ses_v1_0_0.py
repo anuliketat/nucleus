5b97d1499c1d2de22abad22bca407ce81202da8f
Chinni Chaitanya <chchaitanya95@gmail.com>
2019-03-05 18:58:40 +0530

    Change data path to relative and update gitignore

--- api/mancio/ml/ses/ses_v1_0_0.py
+++ api/mancio/ml/ses/ses_v1_0_0.py
@@ -18,8 +18,6 @@ from statsmodels.tsa.api import SimpleExpSmoothing
 import warnings
 
 from ..basic_model import basic_model
-warnings.filterwarnings('ignore')
-os.chdir('D:\\DS tutorial')
 
 class ses_v1_0_0(basic_model):
     def __init__(self):
@@ -27,30 +25,30 @@ class ses_v1_0_0(basic_model):
         self.model_version = 'v1.0.0'
 
     def __get_item_ids__(self, kitchen_id = None):
-        orders = pd.read_csv('orders_data.csv', index_col=0, 
-                dtype={'order_id':object, 
-                'item_id':np.int32, 
-                'name':object, 
+        orders = pd.read_csv('./data/orders_full.csv', index_col=0,
+                dtype={'order_id':object,
+                'item_id':np.int32,
+                'name':object,
                 'quantity':np.int32})
         orders.time = pd.to_datetime(orders.time)
         ids_list = orders.item_id.unique()
         return ids_list
-    
+
     def __get_data__(self, item_id, db_ai, kitchen_id = None, mode = 'daily'):
-        orders = pd.read_csv('orders_data.csv', index_col=0, 
-            dtype={'order_id':object, 
-            'item_id':np.int32, 
-            'name':object, 
+        orders = pd.read_csv('./data/orders_full.csv', index_col=0,
+            dtype={'order_id':object,
+            'item_id':np.int32,
+            'name':object,
             'quantity':np.int32})
         orders.time = pd.to_datetime(orders.time)
-        
+
         item = orders.loc[orders['item_id'] == item_id].groupby('time')[['quantity']].sum()
         if mode == 'weekly':
             data = item.resample('W').sum().fillna(0)
         elif mode == 'monthly':
-            data = item.resample('M').sum().fillna(0)  
+            data = item.resample('M').sum().fillna(0)
         else:
-            data = item.resample('D').sum().fillna(0) 
+            data = item.resample('D').sum().fillna(0)
         train = data[:int(0.9*(len(data)))]
         test = data[int(0.9*(len(data))):]
         return train, test, data
@@ -66,7 +64,7 @@ class ses_v1_0_0(basic_model):
 
     def update_model(self, db_main, db_ai, fs_ai, mode = 'daily'):
         logger('NUCLEUS_MANCIO', 'REQ', 'update_model() called for: {}_{}.'.format(self.model_name, self.model_version))
-        
+
         item_ids = self.__get_item_ids__()
 
         for item_id in item_ids:
@@ -86,8 +84,8 @@ class ses_v1_0_0(basic_model):
 
             residuals = m.resid
             aic_ses = m.aic
-            bic_ses = m.bic 
-            mse = round(mean_squared_error(test.quantity, test_pred), 3) 
+            bic_ses = m.bic
+            mse = round(mean_squared_error(test.quantity, test_pred), 3)
             mae = round(mean_absolute_error(test.quantity, test_pred), 3)
             Mase = round(mase(test.quantity, test_pred), 3)
 
