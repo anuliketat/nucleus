fix: Calculation of mase metric in `mase.py`
Fix the mase `ZeroDivisionError` by checking the length of the data.
Returns `None` if cannot calculate the metric.
This commit also removes rounding of the metrics in all the models
since `round()` function doesn't handle `None` type data.

Fixees #19

utils/mase.py
@@ -5,17 +5,19 @@ def mape(y_true, y_pred): #Mean Absolute Percentage Error
     return np.mean(np.abs((y_true - y_pred)/y_true))*100
 
 def mase(actual, forecast): #Mean Absolute Scaled Error
+    mase = None
     myData = pd.DataFrame({'forecast':forecast, 'actual':actual})
     n = len(myData)
-    naive_diff = []
-    for i in range(len(actual)):
-        if i == 0:
-            temp = actual[i]
-        else:
-            temp = actual[i]-actual[i-1]
-            temp = abs(temp)
-        naive_diff.append(temp)
-    errors = myData['actual'] - myData['forecast']
-    scaledErrors = abs(errors)/(abs(sum(naive_diff))*(1/(n-1)))
-    mase = (abs(scaledErrors).sum())/n
+    if n > 1:
+        naive_diff = []
+        for i in range(len(actual)):
+            if i == 0:
+                temp = actual[i]
+            else:
+                temp = actual[i]-actual[i-1]
+                temp = abs(temp)
+            naive_diff.append(temp)
+        errors = myData['actual'] - myData['forecast']
+        scaledErrors = abs(errors)/(abs(sum(naive_diff))*(1/(n-1)))
+        mase = (abs(scaledErrors).sum())/n
     return mase