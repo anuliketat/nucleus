14443fa62c02c5abbe9ccb7404eb83a8f0106bfa
madhava reddy <you@example.com>
2019-04-02 16:28:24 +0530

    feat: Add fuzzy algorithm to search API
    Add fuzzy algorithm to fetch search results for a query.
    
    Bitbucket issue #43

---
+++ api/search/algorithms/basic_model.py
@@ -0,0 +1,10 @@
+class basic_model(object):
+	def __init__(self):
+		self.model_name = 'basic_model'
+		self.version = 'v1.0.0'
+
+	def get_search_results(self):
+		pass
+
+	def update_model(self):
+		pass
